7e56c844542f0822c9bdd4f6947eae08b98619e9
ARandomProgrammer <luis.henriques@estudante.ufscar.br>
2022-02-14 09:31:26 -0300

    new scripts

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const route = (event) =>{
 };
 
 const routes = {
-  "/TestPageGA4/404": "pages/404.html",
-  "/TestPageGA4/": "pages/index.html",
-  "/TestPageGA4/about": "pages/about.html",
-  "/TestPageGA4/lorem": "pages/lorem.html",
+  "/TestPageGA4/404": "/pages/404.html",
+  "/TestPageGA4/": "/pages/index.html",
+  "/TestPageGA4/about": "/pages/about.html",
+  "/TestPageGA4/lorem": "/pages/lorem.html",
+  "/TestPageGA4/form": "/pages/form.html",
 };
 
 const handleLocation = async () => {
